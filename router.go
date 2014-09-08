fb6414e80ff1f919865585c2c2c4035939d77649
Bryan Weber <bryan@cobenian.com>
2014-09-07 22:21:10 -0400

    removing unused code

--- router.go
+++ router.go
@@ -2,90 +2,10 @@ package main
 
 import (
 	"github.com/brweber2/interwebs/rpvlw"
-//	"github.com/brweber2/interwebs/ipvlw"
 	"fmt"
-//	"log"
 	"log"
 	"github.com/brweber2/interwebs/ipvlw"
 )
-
-//type RouterNic struct {
-//	Addr ipvlw.Address
-//	IO chan ipvlw.Message
-//}
-//
-//func (r RouterNic) Send(m ipvlw.Message) (ipvlw.Message, error) {
-//	r.IO <- m
-//	resp := <- r.IO
-//	return resp, nil
-//}
-//
-//func (r RouterNic) Listen() ([]ipvlw.Message, error) {
-//	resp := <-r.IO
-//	var a []ipvlw.Message
-//	a = append(a, resp)
-//	return a, nil
-//}
-//
-//type RouterData struct {
-//	Running bool
-//	IO chan ipvlw.Message
-//}
-//
-//func (r RouterData) Start() error {
-//	if r.Running {
-//		panic("router is already running")
-//	}
-//	r.Running = true
-//	var routeDb rpvlw.RouteDatabase
-//	for {
-//		if r.Running == false {
-//			break
-//		}
-//
-//		message := <-r.IO
-//		to := message.To()
-//		block, err := containsAddress(routeDb.Routes, to)
-//		if err != nil {
-//			log.Printf("error while looking for the route that contains %v: %v\n", to, err)
-//		}
-//		system := routeDb.Routes[*block]
-//		nic := routeDb.Interfaces[system]
-//		resp, err := nic.Send(message)
-//		if err != nil {
-//			log.Printf("error while trying to send message to %v: %v\n", to, err)
-//		}
-//		if resp != nil {
-//			r.IO <- resp
-//		}
-//
-//	}
-//	return nil
-//}
-//
-//func containsAddress(routes map[ipvlw.Block]rpvlw.System, a ipvlw.Address) (*ipvlw.Block, error) {
-//	for block := range routes {
-//		if block.Contains(a) {
-//			return &block, nil
-//		}
-//	}
-//	return nil, nil
-//}
-//
-//func (r RouterData) Stop() error {
-//	r.Running = false
-//	return nil
-//}
-//
-//func main() {
-//	io := make(chan ipvlw.Message)
-//	routerNic := RouterNic{Addr: ipvlw.Address{1}, IO: io}
-//	fmt.Println(routerNic)
-//	hello := ipvlw.TextMessage{FromAddr: ipvlw.Address{3}, ToAddr: ipvlw.Address{4}, Identifier: 72, Body: "yeah"}
-////	io <-hello
-//	routerNic.Send(hello)
-////	router := rpvlw.Router{System: System{15}, ControlPlane: , DataPlane: }
-//}
 
 func main() {
 	simulateRouting()
@@ -215,13 +135,7 @@ func simulateRouting() {
 
 }
 
-//func MakeNic() rpvlw.Nic {
-//	return nil
-//}
-//
-//func MakeDhcp(routers ... Router) rpvlw.Dhcp {
-//	return nil
-//}
+
 
 
 
--- rpvlw/router.go
+++ rpvlw/router.go
@@ -2,7 +2,6 @@ package rpvlw
 
 import (
 	"github.com/brweber2/interwebs/ipvlw"
-//	"log"
 )
 
 type Router struct {
@@ -11,44 +10,9 @@ type Router struct {
 	DataPlane DataPlane
 }
 
-type RouteDatabase struct {
-	Routes map[ipvlw.Block]System
-	Interfaces map[System]Nic
-	Nics []Nic
-}
-
-type PolicyDatabase struct {
-	Policies []Policy
-}
-
-// **** behavior ****
-
-type Policy interface {
-	Check(a Announcement) bool
-}
-
-type Announcable interface {
-	Announce(a Announcement) error
-	Listen() ([]Announcement, error) // these may be accepted or rejected
-	Announcements() ([]Announcement, error)
-}
-
-type Fiterable interface {
-	AddPolicy(p Policy) error
-	RemovePolicy(p Policy) error
-	Policies() ([]Policy, error)
-}
-
-type Routable interface {
-	Connect(n Nic) error
-	Disconnect(n Nic) error
-	ConnectedRouters() ([]Router, error)
-}
-
 type ControlPlane interface {
-//	Routable
-
 	Runnable
+
 	AddNic(r *Router) error // todo rename!
 	Routers() []*Router
 	AddRoute(s *System, b *ipvlw.Block) error
@@ -58,18 +22,13 @@ type ControlPlane interface {
 
 type DataPlane interface {
 	Runnable
-//	Start() error
-//	Stop() error
 }
 
 type Nic interface {
 	Address() ipvlw.Address
-//	Send(m ipvlw.Message) (ipvlw.Message, error)
-//	Listen() ([]ipvlw.Message, error)
 }
 
 type Dhcp interface {
-//	Register(n Nic) (Router, ipvlw.Address, error)
 	ConnectTo(r *Router, n ... Nic) error
 }
 
